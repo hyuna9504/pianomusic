7e870743be66c403958c851c96eba76c67652591
hyuna9504 <hyuna9509@naver.com>
2017-07-12 11:41:09 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+ <title>PIANO MUSIC</title>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<link href="../css/common.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+<div id="wrap">
+<div id="header">
+	<? include"./lib/top_login1.php"; ?>
+</div>  <!--end of header -->
+
+<div id="menu">
+         <? include"./lib/top_menu1.php"; ?>
+	<div id="login">
+	<? include"./login/login_form.php"; ?></div>
+</div>  <!--end of menu -->
+
+<div id="content">
+       <div id="image">
+			<img src="./img/main2.gif" border="0">
+			</div>
+        
+	</div>
+</div>  <!--end of content-->
+<div id="footer">
+	<? include"./lib/footer.php"; ?>
+</div>  <!--end of footer-->
+</div>  <!--end of wrap-->
+
+<div id="footer">
+
+
+
+</body>
+</html>
